2ed3eb05011c25c8a435f7bc9be6290a22c34b2b
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-01 16:19:12 +0300

    App_Start is refactored according StyleCop rules.

--- src/M101DotNet.WebApp/App_Start/FilterConfig.cs
+++ src/M101DotNet.WebApp/App_Start/FilterConfig.cs
@@ -1,8 +1,7 @@
-﻿using System.Web;
-using System.Web.Mvc;
-
-namespace M101DotNet.WebApp
+﻿namespace M101DotNet.WebApp
 {
+    using System.Web.Mvc;
+
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
--- src/M101DotNet.WebApp/App_Start/Startup.Auth.cs
+++ src/M101DotNet.WebApp/App_Start/Startup.Auth.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Web;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using MongoDB.Driver;
-using Owin;
-
-namespace M101DotNet.WebApp
+﻿namespace M101DotNet.WebApp
 {
+    using Microsoft.Owin;
+    using Microsoft.Owin.Security.Cookies;
+    using Owin;
+
     public partial class Startup
     {
         public void ConfigureAuth(IAppBuilder app)
