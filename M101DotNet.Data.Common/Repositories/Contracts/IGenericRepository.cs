f3ee05292e43879589d633d40230dd3f4c9abf95
Bozhin Karaivanov <bozhink@gmail.com>
2016-05-01 17:31:34 +0300

    IGenericRepository is added to M101DotNet.Data.Common.

---
+++ M101DotNet.Data.Common/Repositories/Contracts/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace M101DotNet.Data.Common.Repositories.Contracts
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IGenericRepository<T>
+        where T : class
+    {
+        Task<IQueryable<T>> All();
+
+        Task<T> Get(object id);
+
+        Task<object> Add(T entity);
+
+        Task<object> Update(T entity);
+
+        Task<object> Delete(T entity);
+
+        Task<object> Delete(object id);
+    }
+}
